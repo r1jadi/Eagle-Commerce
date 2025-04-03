4cd200bbd804d128ae606b78a6033d675a5c453f
Rijad Halili <rijadhalili.yt@gmail.com>
2025-04-03 18:19:05 +0200

    Front end Implementation

---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatBadge} from '@angular/material/badge';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
